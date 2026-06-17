58e57d0f44112a6738cf00994853c34ae65430a7
jchilala50-blip <jchilala50@gmail.com>
2026-06-17 11:19:28 +0100

    Corrige novo OTP

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ try {
     console.log("=== CONTAS DERIV ===");
 console.log(JSON.stringify(contas.data, null, 2));
 
-const accountId = contas.data.data[0].account_id;
+const accountId = 'DOT92096158';
 
 const otp = await axios.post(
     `https://api.derivws.com/trading/v1/options/accounts/${accountId}/otp`,
