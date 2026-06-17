d82e51829f1e92bd77a06631e151748e08f90548
jchilala50-blip <jchilala50@gmail.com>
2026-06-17 17:17:43 +0100

    Corrige refresh OTP dinamico

--- server.js
+++ server.js
@@ -152,7 +152,19 @@ try {
     console.log("=== CONTAS DERIV ===");
 console.log(JSON.stringify(contas.data, null, 2));
 
+const contas = await axios.get(
+    'https://api.derivws.com/trading/v1/options/accounts',
+    {
+        headers: {
+            'Authorization': `Bearer ${derivToken}`,
+            'Deriv-App-ID': CLIENT_ID
+        },
+        timeout: 15000
+    }
+);
+
 const accountId = contas.data.data[0].account_id;
+
 
 const otp = await axios.post(
     `https://api.derivws.com/trading/v1/options/accounts/${accountId}/otp`,
