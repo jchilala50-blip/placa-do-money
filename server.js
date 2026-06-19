3321cc13f5aa25089fd5885741d74e7bbb372903
jchilala50-blip <jchilala50@gmail.com>
2026-06-19 15:26:24 +0100

    Permite alternar conta demo e real

--- server.js
+++ server.js
@@ -152,19 +152,6 @@ try {
 
     console.log("=== CONTAS DERIV ===");
     console.log(JSON.stringify(contas.data, null, 2));
-    
-const contaEscolhida =
-    contas.data.data.find(conta => {
-
-        if (
-            tipoConta === 'REAL'
-        ) {
-            return conta.account_type === 'real';
-        }
-
-        return conta.account_type === 'demo';
-
-    });
 
 const accountId =
     contaEscolhida.account_id;
@@ -238,6 +225,8 @@ app.get('/api/novo-otp', async (req, res) => {
             timeout: 15000
         }
     );
+
+const accountId = contas.data.data[0].account_id;
 
     const otp = await axios.post(
         `https://api.derivws.com/trading/v1/options/accounts/${accountId}/otp`,
