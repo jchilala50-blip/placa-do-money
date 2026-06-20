6188f8193bd7ce1668d16f4e706b5c6c44f71237
jchilala50-blip <jchilala50@gmail.com>
2026-06-20 15:17:13 +0100

    Adiciona debug token e retifica escopo no servidor

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/api/deriv-auth-url', (req, res) => {
         + `?response_type=code`
         + `&client_id=${CLIENT_ID}`
         + `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`
-        + `&scope=trade+account management`
+        + `&scope=trade`
         + `&state=${state}`
         + `&code_challenge=${code_challenge}`
         + `&code_challenge_method=S256`;
@@ -160,6 +160,9 @@ try {
             timeout: 15000
         }
     );
+
+console.log("=== TOKEN USADO ===");
+console.log(token);
 
     console.log("=== CONTAS DERIV ===");
     console.log(JSON.stringify(contas.data, null, 2));
