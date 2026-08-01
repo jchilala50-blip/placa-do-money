99ce39ad5015e217e3110de97fdc9f6cea1f7a3c
jchilala50-blip <jchilala50@gmail.com>
2026-08-01 15:02:55 +0100

    Adiciona botão e janela inicial do sistema de análises

--- analiseUI.js
+++ analiseUI.js
@@ -1,3 +1,56 @@
-function abrirJanelaAnalise() {
-    alert("Sistema de Análises do Mercado\n\nEm desenvolvimento.");
-}
+(function () {
+
+let janela = null;
+
+window.abrirJanelaAnalise = function () {
+
+    if (janela) {
+        janela.style.display = "block";
+        return;
+    }
+
+    janela = document.createElement("div");
+
+    janela.id = "janela-analise";
+
+    janela.innerHTML = `
+        <div id="topo-analise">
+
+            <span id="titulo-analise">
+                SISTEMA DE ANÁLISES DO MERCADO
+            </span>
+
+            <span id="fechar-analise">
+                ✖
+            </span>
+
+        </div>
+
+        <div id="status-live">
+
+            <span id="led-live"></span>
+
+            <span>LIVE</span>
+
+        </div>
+
+        <div id="conteudo-analise">
+
+            Em desenvolvimento...
+
+        </div>
+    `;
+
+    document.body.appendChild(janela);
+
+    document
+        .getElementById("fechar-analise")
+        .onclick = function () {
+
+            janela.style.display = "none";
+
+        };
+
+};
+
+})();
